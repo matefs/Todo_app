7f61c1f265d08c6666e517f204a63437f0710464
Mateus Schverz <mate_fs@hotmail.com>
2023-05-20 22:58:46 -0300

    add: componente listTIle com o texto do todo item

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class ToDoItem extends StatelessWidget {
   const ToDoItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Item ToDo'),);
+    return Container(
+      child: ListTile(
+        onTap: (){},
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        tileColor: Colors.white,
+        leading: Icon(Icons.check_box, color: tdBlue,),
+        title: Text('Check mail', style: TextStyle(fontSize: 16,color: tdBlack,decoration: TextDecoration.lineThrough),),
+      ),
+    );
   }
 }
