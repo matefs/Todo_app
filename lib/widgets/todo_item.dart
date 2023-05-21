adfef247a9f680d32b1ec3dbef2e37898a16b07e
Mateus Schverz <mate_fs@hotmail.com>
2023-05-20 23:54:38 -0300

    add: condicionais de renderização para checkbox e text decoration
    caso o todo não estiver setado como concluido, não irá aparecer o checkbox de marcado e nem o texto como estando tachado (cortado no meio)

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -18,10 +18,16 @@ class ToDoItem extends StatelessWidget {
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         contentPadding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
         tileColor: Colors.white,
-        leading: Icon(Icons.check_box, color: tdBlue,),
+        leading: Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlue,),
         title: Text(
           todo.todoText!,
-          style: TextStyle(fontSize: 16,color: tdBlack,decoration: TextDecoration.lineThrough),),
+          style: TextStyle(
+              fontSize: 16,
+              color: tdBlack,
+              decoration: todo.isDone ? TextDecoration.lineThrough : TextDecoration.none ),
+        ),
         trailing: Container(
           height: 35,
           width: 35,
