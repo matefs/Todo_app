3528cd7b0277ebf3eebebaad90b6b3bd6c03361e
Mateus Schverz <mate_fs@hotmail.com>
2023-05-21 16:14:53 -0300

    feat: adicionado funcao de pesquisar todos

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,7 +3,6 @@ import 'package:todo_app/constants/colors.dart';
 import '../model/todo.dart';
 import 'package:todo_app/widgets/todo_item.dart';
 
-
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
 
@@ -13,7 +12,14 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final todosList = ToDo.todoList();
+  List<ToDo> _foundToDo = [];
   final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    _foundToDo = todosList;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,31 +29,28 @@ class _HomeState extends State<Home> {
       body: Stack(
         children: [
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 24,vertical: 15),
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 15),
             child: Column(
               children: [
                 searchBox(),
                 Expanded(
                     child: ListView(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(top:50,bottom: 20),
-                        child: Text('Todos os todos', style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w500),)
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          'Todos os todos',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w500),
+                        )),
+                    for (ToDo todoo in _foundToDo)
+                      ToDoItem(
+                        todo: todoo,
+                        onTodoChanged: _handleTodoChange,
+                        onDeleteItem: _deleteToDoItem,
                       ),
-
-                      for( ToDo todoo in todosList)
-                        ToDoItem(
-                          todo: todoo,
-                          onTodoChanged: _handleTodoChange,
-                          onDeleteItem: _deleteToDoItem,
-                        ),
-
-
                   ],
-                  )
-                )
+                ))
               ],
             ),
           ),
@@ -55,46 +58,46 @@ class _HomeState extends State<Home> {
             alignment: Alignment.bottomCenter,
             child: Row(
               children: [
-                Expanded(child: Container(
+                Expanded(
+                    child: Container(
                   margin: EdgeInsets.only(
-                    bottom:20,
+                    bottom: 20,
                     left: 20,
                     right: 20,
                   ),
                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    boxShadow: const [BoxShadow(
+                    boxShadow: const [
+                      BoxShadow(
                         color: Colors.grey,
-                        offset: Offset(0.0,0.0),
-                      blurRadius: 10.0,
-                      spreadRadius: 0.0,
-
-                    ),],
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: TextField(
-                    controller: _todoController,
-                    decoration: InputDecoration(
-                      hintText: "Adicione um novo item todo",
-                      border: InputBorder.none,
-                    )
-                  ),
-                )
-                ),
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                        hintText: "Adicione um novo item todo",
+                        border: InputBorder.none,
+                      )),
+                )),
                 Container(
                   margin: EdgeInsets.only(
                     bottom: 20,
                     right: 20,
                   ),
                   child: ElevatedButton(
-                    child: Text('+',style: TextStyle(fontSize: 40)),
-                    onPressed: (){
+                    child: Text('+', style: TextStyle(fontSize: 40)),
+                    onPressed: () {
                       _addTodoItem(_todoController.text);
                     },
                     style: ElevatedButton.styleFrom(
                       primary: tdBlue,
-                      minimumSize: Size(60,60),
+                      minimumSize: Size(60, 60),
                       elevation: 10,
                     ),
                   ),
@@ -107,57 +110,76 @@ class _HomeState extends State<Home> {
     );
   }
 
-  void _handleTodoChange(ToDo todo){
+  void _handleTodoChange(ToDo todo) {
     setState(() {
       todo.isDone = !todo.isDone;
     });
   }
 
-  void _deleteToDoItem(String id){
+  void _deleteToDoItem(String id) {
     setState(() {
       todosList.removeWhere((item) => item.id == id);
     });
   }
 
-  void _addTodoItem(String toDo){
+  void _addTodoItem(String toDo) {
     setState(() {
-      todosList.add(ToDo(id: DateTime.now().millisecondsSinceEpoch.toString(), todoText: toDo));
+      todosList.add(ToDo(
+          id: DateTime.now().millisecondsSinceEpoch.toString(),
+          todoText: toDo));
     });
     _todoController.clear();
   }
 
-  Widget searchBox(){
+  void _runFilter(String enteredKeyWord) {
+    List<ToDo> results = [];
+    if (enteredKeyWord.isEmpty) {
+      results = todosList;
+    } else {
+      results = todosList
+          .where((item) => item.todoText!
+              .toLowerCase()
+              .contains(enteredKeyWord.toLowerCase()))
+          .toList();
+    }
+    setState(() {
+      _foundToDo = results;
+    });
+  }
+
+  Widget searchBox() {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 15),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20),
       ),
-      child: const TextField(
-        decoration: InputDecoration(
+      child: TextField(
+        onChanged: (value) => _runFilter(value),
+        decoration: const InputDecoration(
           contentPadding: EdgeInsets.all(8.0),
-          prefixIcon: Icon(
-              Icons.search,
-              color: tdBlack,
-              size:20
-          ),
+          prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
           prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
           border: InputBorder.none,
           hintText: "Pesquisar...",
-          hintStyle: TextStyle(color:tdGrey),
+          hintStyle: TextStyle(color: tdGrey),
         ),
       ),
     );
   }
 
-  AppBar __buildAppBar(){
+  AppBar __buildAppBar() {
     return AppBar(
       elevation: 0,
       backgroundColor: tdBGColor,
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Icon(Icons.menu,color: tdBlack,size: 30,),
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
           Container(
             height: 40,
             width: 40,
@@ -166,7 +188,8 @@ class _HomeState extends State<Home> {
               child: Image.asset('assets/images/avatar.jpeg'),
             ),
           )
-        ],),
+        ],
+      ),
     );
   }
 }
